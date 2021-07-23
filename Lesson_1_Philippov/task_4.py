50e671734b3b91482e9be81c68b402729972bfe5
philippov <bervr@mail.ru>
2021-07-23 17:59:33 +0400

    6 done

--- Lesson_1_Philippov/task_4.py
+++ Lesson_1_Philippov/task_4.py
@@ -8,3 +8,19 @@
 Подсказки:
 --- используйте списки и циклы, не дублируйте функции
 """
+def en_de_code(items, direction):
+    result=[]
+    for item in items:
+        if direction == 'encode':
+            result.append(item.encode('utf-8'))
+        elif direction == 'decode':
+            result.append(item.decode('utf-8'))
+    print(result)
+    return result
+
+
+if __name__ == "__main__":
+    my_list = ['разработка', 'администрирование', 'protocol','standard']
+
+    my_list = en_de_code(my_list, 'encode')
+    my_list = en_de_code(my_list, 'decode')
