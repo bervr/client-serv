3156573173c77756daf8c7dfc17f8b3af0f57afe
bervr <bervrr@gmail.com>
2021-07-24 10:02:51 +0400

    5 done

--- Lesson_1_Philippov/task_5.py
+++ Lesson_1_Philippov/task_5.py
@@ -7,3 +7,20 @@
 Подсказки:
 --- используйте модуль chardet, иначе задание не засчитается!!!
 """
+
+import subprocess
+import chardet
+
+def run_ping(args):
+    for item in args:
+        # print(item)
+        new_ping = subprocess.Popen(item, stdout=subprocess.PIPE)
+        for line in new_ping.stdout:
+            result = chardet.detect(line)
+            line = line.decode(result['encoding'])  #  .encode('utf-8')
+            print(line)  #   .decode('utf-8'))
+
+
+
+ARGS =(['ping', 'localhost', '-c 4'],['ping', 'ya.ru', '-c 4'])
+run_ping(ARGS)
