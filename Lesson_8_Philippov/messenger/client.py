58afe8d418a5eebc0b61ddb55bc0232580fca6ef
philippov <bervr@mail.ru>
2021-08-18 17:33:13 +0400

    threads on client

--- Lesson_8_Philippov/messenger/client.py
+++ Lesson_8_Philippov/messenger/client.py
@@ -53,28 +53,50 @@ class MsgClient:
             return f'400 : {message[ERROR]}'
         raise errors.ReqFieldMissingError(RESPONSE)
 
+    # def __init__(self):
+    #     LOGGER.debug("Инициализация клиента")
+    def client_sending(self):
+        LOGGER.info('Режим работы - отправка сообщений')
+        while True:
+            try:
+                send_message(self.transport, self.create_message(self.client_name))
+            except (ConnectionError, ConnectionResetError, ConnectionAbortedError):
+                LOGGER.error(f'Соединение с сервером {self.server_address} было утеряно')
+                sys.exit(1)
+
+    def client_receiving(self):
+        LOGGER.info('Режим работы - прием сообщений')
+        while True:
+            try:
+                answer = get_message(self.transport)
+                print(f'User{answer["sender"]}: {answer["message_text"]}')
+                LOGGER.info(f'Сообщение из чята от {answer["sender"]}: {answer["message_text"]}')
+                print(f'Сообщение из чята от {answer["sender"]}: {answer["message_text"]}')
+            except (ConnectionError, ConnectionResetError, ConnectionAbortedError):
+                LOGGER.error(f'Соединение с сервером {self.server_address} было утеряно')
+                sys.exit(1)
+
     def start(self):
-        # def __init__(self):
         # получаем параметры из командной строки
         # client.py -a localhost -p 8079 -m send/listen
         LOGGER.debug("Запуск клиента")
         parser = create_arg_parser()
         namespace = parser.parse_args(sys.argv[1:])
-        server_address = namespace.a
-        server_port = namespace.p
+        self.server_address = namespace.a
+        self.server_port = namespace.p
         client_mode = namespace.m
-        LOGGER.debug(f'Адрес и порт сервера {server_address}:{server_port}')
+        LOGGER.debug(f'Адрес и порт сервера {self.server_address}:{self.server_port}')
         try:
             self.transport = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-            self.transport.connect((server_address, server_port))
-            # print(self.transport)
+            self.transport.connect((self.server_address, self.server_port))
             print(self.transport.getsockname()[1])
+            self.client_name = self.transport.getsockname()[1]
             LOGGER.debug(
-                f'Подключение к серверу с адресом {server_address if server_address else "localhost"} '
-                f'по {server_port} порту')
+                f'Подключение к серверу с адресом {self.server_address if self.server_address else "localhost"} '
+                f'по {self.server_port} порту')
         except ConnectionRefusedError:
-            LOGGER.error(f'Не удалось подключится к серверу {server_address}:{server_port}, '
-                         f'возможн он не запущен или что-то с сетью')
+            LOGGER.error(f'Не удалось подключится к серверу {self.server_address}:{self.server_port}, '
+                         f'возможно он не запущен или что-то с сетью')
         except json.JSONDecodeError:
             LOGGER.error('Не удалось декодировать полученную Json строку.')
             sys.exit(1)
@@ -85,37 +107,22 @@ class MsgClient:
             LOGGER.error(f'В ответе сервера отсутствует необходимое поле {missing_error.missing_field}')
             sys.exit(1)
 
-        message_to_server = self.create_presence()
+        message_to_server = self.create_presence(self.client_name)
         LOGGER.info(f'Отправка сообщения на сервер - {message_to_server}')
         send_message(self.transport, message_to_server)
         try:
             answer = self.process_ans(get_message(self.transport))
-            # print(answer)
             LOGGER.info(f'Получен ответ от сервера {answer}')
         except (ValueError, json.JSONDecodeError):
             # print('Не удалось декодировать сообщение сервера.')
             LOGGER.critical(f'Не удалось декодировать сообщение от сервера')
 
-        if client_mode == 'send':
-            LOGGER.info('Режим работы - отправка сообщений')
-        else:
-            LOGGER.info('Режим работы - прием сообщений')
-        while True:
-            if client_mode == 'send':
-                try:
-                    send_message(self.transport, self.create_message())
-                except (ConnectionError, ConnectionResetError, ConnectionAbortedError):
-                    LOGGER.error(f'Соединение с сервером {server_address} было утеряно')
-                    sys.exit(1)
-
-            if client_mode == 'listen':
-                try:
-                    answer = get_message(self.transport)
-                    LOGGER.info(f'Сообщение из чята от {answer["sender"]}: {answer["message_text"]}')
-                    print(f'Сообщение из чята от {answer["sender"]}: {answer["message_text"]}')
-                except (ConnectionError, ConnectionResetError, ConnectionAbortedError):
-                    LOGGER.error(f'Соединение с сервером {server_address} было утеряно')
-                    sys.exit(1)
+        send_thread = Thread(target=self.client_sending, daemon=True)
+        receive_thread = Thread(target=self.client_receiving, daemon=True)
+        send_thread.start()
+        receive_thread.start()
+        send_thread.join()
+        receive_thread.join()
 
 
 if __name__ == '__main__':
