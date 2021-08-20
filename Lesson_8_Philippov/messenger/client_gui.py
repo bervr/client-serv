6df540ae688b38e667bbd84b93ce6f2e5b378d94
philippov <bervr@mail.ru>
2021-08-20 16:56:53 +0400

    gui render started

--- Lesson_8_Philippov/messenger/client_gui.py
+++ Lesson_8_Philippov/messenger/client_gui.py
@@ -8,30 +8,31 @@ from kivy.app import runTouchApp
 from kivy.uix.textinput import TextInput
 from functools import partial
 
-class NewClient(MsgClient):
+# class NewClient(MsgClient):
 
-    def create_layout(self):
-        # добавили основной слой
-        layout = GridLayout(cols=1, spacing=10, size_hint_y=None)
-        layout.bind(minimum_height=layout.setter('height'))
+def create_layout():
+    # добавили основной слой
+    layout = GridLayout(cols=1, spacing=10, size_hint_y=None)
+    layout.bind(minimum_height=layout.setter('height'))
 
-        #добавили поле ввода текста
-        msg_input = TextInput(size_hint_y=None, height=50, multiline=True, hint_text='Напишите здесь что-нибудь')
-        layout.add_widget(msg_input)
+    #добавили поле ввода текста
+    msg_input = TextInput(size_hint_y=None, height=50, multiline=True, hint_text='Напишите здесь что-нибудь')
+    layout.add_widget(msg_input)
 
-        #добавили кнопку отправки
-        send_button = Button(text='отправить!', size_hint_y=None, height=120)
-        # send_button.on_press = partial(self.send)
-        layout.add_widget(send_button)
+    #добавили кнопку отправки
+    send_button = Button(text='отправить!', size_hint_y=None, height=120)
+    # send_button.on_press = partial(self.send)
+    layout.add_widget(send_button)
 
-        root = ScrollView(size_hint=(1, None), size=(Window.width, Window.height))
-        runTouchApp(root)
+    root = ScrollView(size_hint=(1, None), size=(Window.width, Window.height))
+    root.add_widget(layout)
+    runTouchApp(root)
 
 
 
 
 
 if __name__ == '__main__':
-    client = NewClient()
-    client.start()
-    client.create_layout()
+    # client = NewClient()
+    # client.start()
+    create_layout()
