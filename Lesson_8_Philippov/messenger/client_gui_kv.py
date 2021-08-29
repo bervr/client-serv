d9a5584d10d6a0d7f040d190edfe3de4b80d5bfb
bervr <bervrr@gmail.com>
2021-08-29 13:33:06 +0400

    with  startings hreads in gui

--- Lesson_8_Philippov/messenger/client_gui_kv.py
+++ Lesson_8_Philippov/messenger/client_gui_kv.py
@@ -1,5 +1,7 @@
 import sys
 import time
+from threading import Thread
+
 from kivy.app import App
 from kivy.uix.screenmanager import ScreenManager, Screen, FadeTransition
 from kivy.lang import Builder
@@ -33,16 +35,21 @@ class SecondScreen(Screen):
     def __init__(self, new_msg, **kwargs):
         super().__init__(**kwargs)
         self.msg_obj = new_msg
-        self.clients = [123, 456, 789, 987, 654, 321, 505, 18, 14, 45]
-        # self.clients = []
+        # self.clients = [123, 456, 789, 987, 654, 321, 505, 18, 14, 45]
+        # # self.clients = []
 
     def check_name(self):
+        """проверяем что это имя не занято"""
         print(self.msg_obj.client_name)
         answer = self.msg_obj.hello(self.msg_obj.client_name)
         if answer != RESPONSE_200:
             self.revert()
+        else:
+            self.msg_obj.start_threads()
+            return
 
     def revert(self):
+        """ возвращает на экран логина"""
         print('имя уже занято')  # todo  починить надпись о том что имя уже занято
         # self.parent.screens[0].login.text = ''
         # self.parent.screens[0].login.hint_text = f"[color=363636]Нельзя зарегистрироваться под именем  " \
@@ -50,18 +57,18 @@ class SecondScreen(Screen):
         self.manager.current = 'first'
 
     def render_contacts(self):
-        self.update_userlist()
-        print(self.msg_obj.remote_users)
-
+        """ загружаем с сервера и перерисовываем контакты"""
+        # self.update_userlist()
+        self.msg_obj.get_clients()
+        # print(self.msg_obj.remote_users)
         for i in range(len(self.ids.contacts.children)):
-
             self.ids.contacts.remove_widget(self.ids.contacts.children[-1])
 
         self.your_name.text = f"[color=000000]Вы видны всем под именем: " \
                               f"{self.msg_obj.client_name if self.msg_obj.client_name else 'Guest'}[/color]"
 
-        if self.clients !=[]:
-            for i in self.clients:
+        if self.msg_obj.remote_users != []:
+            for i in self.msg_obj.remote_users:
                 self.contacts.add_widget(
                     Button(text=f'{i}', size_hint_y=None, height=40, on_press=self.select_user)
                 )
@@ -71,11 +78,10 @@ class SecondScreen(Screen):
         self.manager.current = 'first'
 
     def select_user(self, instance):
+        """ выбор контакта с которым бутем переписываться, подгрузка его истории"""
+        self.msg_obj.destination = instance.text
         print(instance.text)
-        print(self.msg_obj.client_name)
-
-    def update_userlist(self):
-        self.msg_obj.get_clients()
+        # print(self.msg_obj.client_name)
 
 
 class MyMsg(MsgClient):
@@ -84,6 +90,7 @@ class MyMsg(MsgClient):
         self.destination = None
         self.message = ''
         self.to_send = False
+        self.history = {}  # {contact:{1:}}
 
     def create_message(self):
         out = {
@@ -100,13 +107,14 @@ class MyMsg(MsgClient):
         return out
 
     def client_receiving(self):
+        LOGGER.info('Режим работы - прием сообщений')
         while True:
             try:
                 answer = get_message(self.transport)
                 print(answer)
                 if RESPONSE in answer:
                     self.process_ans(answer)
-                    print(self.remote_users)
+                    # print(self.remote_users)
                 else:
                     print(f'\nUser {answer[SENDER]} sent: {answer[USER][MESSAGE_TEXT]}')
                     LOGGER.info(f'Сообщение из чята от {answer[SENDER]}: {answer[USER][MESSAGE_TEXT]}')
@@ -126,28 +134,39 @@ class MyMsg(MsgClient):
                     LOGGER.error(f'Соединение с сервером {self.server_address} было утеряно')
                     sys.exit(1)
 
+    def start_threads(self):
+        """ переопределили родительский метод чтобы программа не блокировалась при запуске слушателей"""
+        receive_thread = Thread(target=self.client_receiving, daemon=True)
+        send_thread = Thread(target=self.client_sending, daemon=True)
+        receive_thread.start()
+        send_thread.start()
+        # receive_thread.join()
+        # send_thread.join()
+        # return
+
+
 class ClientApp(App):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
-        # new_msg.start_threads()
-
-
     def build(self):
+        """ выстраиваем интерфейс"""
         # Добавляю красивый переход FadeTransition
         sm = ScreenManager(transition=FadeTransition())  # Создаю менеджер экранов sm
         # обязательно нужно дать имя экрану, ведь по этому имени и будет производиться переключение
         # в kv файле для преключения нужно использовать root.manager.current, а в коде self.manager.current
-        new_msg = MyMsg()
+        self.new_msg = MyMsg()
         # new_msg = MsgClient()
-        first_screen = FirstScreen(new_msg, name='first')
-        second_screen = SecondScreen(new_msg, name='second')
+
+        first_screen = FirstScreen(self.new_msg, name='first')
+        second_screen = SecondScreen(self.new_msg, name='second')
         sm.add_widget(first_screen)
         sm.add_widget(second_screen)
-        # new_msg.start_threads()
+        self.title = 'MeowMessenger'
+        # self.icon = 'myicon.png'
         return sm
 
 
 if __name__ == "__main__":
     ClientApp().run()
-    # ClientApp().new_msg.start_threads()
+    # ClientApp().run_threads()
